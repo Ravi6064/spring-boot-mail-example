167e9ce7f198ebae927dcd7524b10632ab6f0c48
Ravi6064 <35137207+Ravi6064@users.noreply.github.com>
2022-09-13 22:10:00 +0530

    Update SpringBootEmailExampleApplication.java

--- src/main/java/com/airtel/springbootemailexample/SpringBootEmailExampleApplication.java
+++ src/main/java/com/airtel/springbootemailexample/SpringBootEmailExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootEmailExampleApplication {
 
 	public static void main(String[] args) {
+				System.out.println("Hello");
 		SpringApplication.run(SpringBootEmailExampleApplication.class, args);
 	}
 
